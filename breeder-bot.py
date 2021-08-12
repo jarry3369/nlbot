4f1c16f7b8d6a11b99e426bd90e47839928ebe58
jarry3369 <mac2009@naver.com>
2021-08-13 02:47:59 +0900

    Update breeder-bot.py

--- breeder-bot.py
+++ breeder-bot.py
@@ -6,6 +6,7 @@ import requests
 from requests.api import request
 from requests.sessions import Request
 import os
+import random
 
 
 url = 'https://script.google.com/macros/s/AKfycbwIGjc6merW4KOIh_fQ1hTOLw28nB_B0iRHiWDJG103gBskGHLVIVjb_s_YZIiPfSUW6w/exec?sheetName=requestList'
@@ -68,11 +69,16 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
+    foodList = ['사탕','초콜릿','물','짜장면','짬뽕','치킨','족발','라면','피자','파스타','돈까스','김치볶음밥','양꼬치','돼지국밥','마라탕','순대국밥','떡볶이','갈비탕','냉면','밀면','쌀국수','볶음밥','삼겹살','소고기','회','초밥','우동','쫄면','육회','찜닭','감자탕','커피','햄버거','김밥','보쌈','라멘','비빔밥','아무거나','곰젤리','콜라','사이다','맥주','쭈꾸미','곱창','갈비찜','소바','부대찌개','김치찌개','된장찌개','스테이크','빵','츄러스','치즈케이크','샐러드','요거트','죽','덮밥','샤브샤브','월남쌈','전골']
+    menu = random.choice(foodList) 
     output = requests.get(url).json()
 
     if message.author == client.user: # 봇 메시지 무시
         return
 
+    if message.content == ';밥':
+        await client.channel.send(menu+"ㄱㄱㄱ")
 
     if message.content.startswith(';arthro'):
         checkList(output,arthro)
