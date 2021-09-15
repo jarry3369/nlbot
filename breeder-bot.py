9f7d27098f9fe5841f2477a7f29945ad1f9dcd8c
jarry3369 <mac2009@naver.com>
2021-09-15 12:15:51 +0900

    Update breeder-bot.py

--- breeder-bot.py
+++ breeder-bot.py
@@ -70,16 +70,6 @@ async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game("곰젤리 먹방"))
 
     print('We have logged in as {0.user}'.format(client)) #실행 콘솔로그
-    await patchLog()
-
-async def patchLog(message):
-    await client.wait_until_ready()
-    ch = client.get_channel('chanID')
-
-    while not client.is_closed():
-        while 1:
-            await message.ch.send(patchOut)
-            await asyncio.sleep(60)
 
 
 @client.event
@@ -88,12 +78,19 @@ async def on_message(message):
     foodList = ['사탕','초콜릿','물','짜장면','블루베리','짬뽕','치킨','족발','라면','피자','파스타','돈까스','김치볶음밥','양꼬치','돼지국밥','마라탕','순대국밥','떡볶이','갈비탕','냉면','밀면','쌀국수','볶음밥','삼겹살','소고기','회','초밥','우동','쫄면','육회','찜닭','감자탕','커피','햄버거','김밥','보쌈','라멘','비빔밥','아무거나','곰젤리','콜라','사이다','맥주','쭈꾸미','곱창','갈비찜','소바','부대찌개','김치찌개','된장찌개','스테이크','빵','츄러스','치즈케이크','샐러드','요거트','죽','덮밥','샤브샤브','월남쌈','전골']
     menu = random.choice(foodList) 
     output = requests.get(url).json()
+    ch = client.get_channel('chanID')
 
     if message.author == client.user: # 봇 메시지 무시
         return
 
     if message.content == ';밥':
         await message.channel.send(menu+"ㄱㄱㄱ")
+    
+    while not client.is_closed():
+        msg = await message.ch.send(patchOut)
+        while 1:
+            await asyncio.sleep(60)
+            await msg.edit(patchOut)
 
     # if message.content.startswith(';arthro'):
     #     checkList(output,arthro)
